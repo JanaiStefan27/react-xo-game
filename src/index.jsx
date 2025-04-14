7991ce72abd16d24306f903a0b062417cd6c016d
JanaiStefan27 <janai.stefan122797@gmail.com>
2025-04-14 21:19:27 +0300

    Added base pages files + store file and create context + instaled styled chit

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import GameBoardPage from "./pages/game-board.page";
+import GameMenuPage from "./pages/game-menu.page";
+import RegisterPage from "./pages/register.page";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Index />
+    {/* Tip: It provides specific hooks and data global accessible within children components */}
+    <BrowserRouter>
+      {/* Tip: The Routes must be parent of any set of routes */}
+      <Routes>
+        {/* Tip: The Toure helps us to create a page */}
+        <Route index={true} element={<RegisterPage />} />
+        {/* Tip: nested routes helps use to group pages under specifi prefixes */}
+        <Route path="/game">
+          {/* Tip: We can acces the page under "/game/menu" */}
+          <Route path="menu" element={<GameMenuPage />} />
+          <Route path="board" element={<GameBoardPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
---
+++ src/pages/register.page.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const RegisterPage = () => {
+  return (
+    <Container>
+      <p>Game menu page</p>
+    </Container>
+  );
+};
+
+const Container = styled.main`
+  display: flex;
+`;
+
+export default RegisterPage;
